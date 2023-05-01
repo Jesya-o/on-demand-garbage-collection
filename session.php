a1c046e9b89753f366ed21cfda0fcff4a5b7adc5
Ksenija Okuneva <ksokun@ttu.ee>
2023-05-01 12:00:57 +0300

    session ended: log in again - custom message

--- session.php
+++ session.php
@@ -16,7 +16,54 @@ if (
     !($clientKey = $_SESSION['client_key'] ?? null) ||
     !validateClientKey($clientKey)
 ) {
-    echo "<script>alert('Please log in again.'); window.location.href='login.php';</script>";
+    echo "<script>
+        const homeBtn = document.getElementById('homeBtn');
+        const loginBtn = document.getElementById('loginBtn');
+
+        homeBtn.addEventListener('click', () => {
+            window.location.href = 'index.php'; // Replace with your home page URL
+        });
+
+        loginBtn.addEventListener('click', () => {
+            window.location.href = 'login.php';
+        });
+    </script>";
+    echo "<div id='customAlert' style='    
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        padding: 20px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        z-index: 9999;
+    '>
+            <p style='text-align:center'>Please log in again</p>
+            <button id='homeBtn' style='
+                background-color: white;
+                color: black;
+                border: 1px solid #404040;
+                border-radius: 10px;
+                padding: 10px 15px;
+                margin: 5px;
+                cursor: pointer;
+            '>
+            Home
+            </button>
+            <button id='loginBtn' style='    
+                background-color: #404040;
+                color: white;
+                border: none;
+                border-radius: 10px;
+                padding: 10px 15px;
+                margin: 5px;
+                cursor: pointer;
+            '>
+            Log in
+            </button>
+          </div>";
     header("Cache-Control: no-cache, must-revalidate");
     header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
     exit;
